6b51a396dc920289e163284ea05e036197c4f086
Augusto Souza <augustojsouza1@gmail.com>
2021-09-22 15:02:52 -0300

    Writing file for the users events

--- src/controllers/puppeteer.controller.js
+++ src/controllers/puppeteer.controller.js
@@ -1,4 +1,14 @@
+const fs = require('fs');
+
 exports.createPuppeteerEvent = async (req, res) => {
+    const data = JSON.stringify(req.body);
+
+    fs.appendFile("UserEvents.txt", data, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+        console.log("The file was saved!");
+    }); 
+
     console.log(req.body);
-    res.status(200).json({ success: true, data: req.body });
 };
